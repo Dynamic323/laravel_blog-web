fe812b4f65a0bf8aafc396d7394f82f4401a197f
John Doe <johndoe@example.com>
2024-09-23 20:13:39 +0100

    "Updated namespace case for Hero view component from 'Hero' to 'hero'"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
-use App\View\Components\Hero;
+use App\View\Components\hero;
 
 class AppServiceProvider extends ServiceProvider
 {
