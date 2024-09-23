4025d6a84be5dc247e3306cd66ef4e0a44d0e688
John Doe <johndoe@example.com>
2024-09-23 19:52:06 +0100

    "Updated namespace for hero component in User home view"

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-views.Components.hero />
+    <x-Components.hero />
 
     <div class="container mx-auto px-4 py-8">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
